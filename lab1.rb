8ba749622076a43e011b77177ac64c4ec75d7e9f
addisonz <addisonz@hd8275tz@my.hennepintech.edu>
2016-11-01 10:19:40 -0500

    finished ruby lab1 script

--- lab1.rb
+++ lab1.rb
@@ -1,5 +1,5 @@
 ##############################################################################
-# 
+#
 # Update this script to:
 #
 # - Ask for the person's name
@@ -11,3 +11,13 @@
 # - Thank the person by name for stopping by
 #
 #############################################################################
+puts "What is your name?"
+name = gets.chomp
+puts 'Hello ' + name
+puts "What is your age?"
+age = gets.chomp
+puts (525600 * age.to_i).to_s + ' is your age in minutes'
+puts 'what is the temperature in Fahrenheit'
+temperature = (gets.chomp)
+puts ((temperature.to_i - 32)/ 1.8 ).to_s + ' is the temperature in celcius'
+puts 'thank you for stopping by ' + name
